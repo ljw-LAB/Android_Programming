64b650d0ca58f0a1914de7f0f2ccd901131d01f3
LEE-NOTEBOOK\이재웅 <su5099@naver.com>
2021-07-21 22:15:12 +0900

    Kotlin 13th Commit  - 02.Listener ~ 05.Task, OpenInternet.kt

---
+++ 01.Study/fastcampus/My Application/app/src/main/java/com/example/myapplication/OpenInternet.kt
@@ -0,0 +1,43 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import androidx.core.widget.addTextChangedListener
+import kotlinx.android.synthetic.main.activity_open_internet.*
+import kotlinx.android.synthetic.main.activity_intent.*
+
+class OpenInternet : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_open_internet)
+        //address_edit_text
+        open.setOnClickListener{
+            val address = address_edit_text.text.toString()
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse((address)))
+            startActivity(intent)
+        }
+
+        address_edit_text.addTextChangedListener(object  : TextWatcher{
+            override fun afterTextChanged(s: Editable?) {
+                Log.d("edit","afterTextChanged : " + s)
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                Log.d("edit","beforeTextChanged : " + s)
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                Log.d("edit","onTextChanged : " + s)
+
+            }
+
+        })
+
+    }
+}
