1d9499f7f0a3db7848b8c77d057a2fcece32d382
ljw-LAB <su5099@naver.com>
2021-07-27 19:03:59 +0900

    Kotlin 17th Commit  - PhoneBookWithRecyclerViewActivity commit

--- 01.Study/fastcampus/My Application/app/src/main/java/com/example/myapplication/RecycleViewActivity.kt
+++ 01.Study/fastcampus/My Application/app/src/main/java/com/example/myapplication/RecycleViewActivity.kt
@@ -2,10 +2,13 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_recycle_view.*
@@ -22,6 +25,7 @@ class RecyclerViewActivity : AppCompatActivity() {
         val adapter = RecyclerViewAdapter(carList, LayoutInflater.from(this@RecyclerViewActivity))
         recycler_view.adapter = adapter
         recycler_view.layoutManager = LinearLayoutManager(this@RecyclerViewActivity)
+        //recycler_view.layoutManager = GridLayoutManager(this@RecyclerViewActivity, 2)
     }
 }
 
@@ -29,7 +33,7 @@ class RecyclerViewAdapter(
     val itemList : ArrayList<CarForList>,
     val inflater: LayoutInflater
 ) : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>(){
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
     {
         val carName : TextView
         val carEngine : TextView
@@ -37,6 +41,12 @@ class RecyclerViewAdapter(
         init{
             carName = itemView.findViewById(R.id.car_name)
             carEngine = itemView.findViewById(R.id.car_engine)
+            itemView.setOnClickListener{
+                val position : Int = adapterPosition
+                val engineName = itemList.get(position).engine
+                Log.d("engine", engineName)
+
+            }
 
         }
 
