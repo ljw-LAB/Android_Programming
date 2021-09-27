8149be7b8c37b6eef6becb70e7fb26495afb0581
LEE-NOTEBOOK\이재웅 <su5099@naver.com>
2021-09-27 23:20:12 +0900

    Kotlin 12th Commit  - Kotlin Programming  Project Commit(06.01)

---
+++ 01.Study/Textbook1/Ch06/src/section1/GetterSetterTest.kt
@@ -0,0 +1,16 @@
+package section1
+
+class User(_id : Int, _name : String, _age:Int)
+{
+    val id : Int = _id
+    var name : String = _name
+    var age : Int = _age
+}
+
+fun main()
+{
+    val user = User(1, "Sean", 30)
+    val name = user.name
+    user.age = 41
+    println("name : $name, ${user.age}")
+}
---
+++ 01.Study/Textbook1/Ch06/src/section1/NormalGetterSetter.kt
@@ -0,0 +1,28 @@
+package section1.getsetter
+
+class User(_id : Int, _name : String, _age:Int)
+{
+    val id : Int = _id
+    get() = field
+
+    var name : String = _name
+    get() = field
+    set(value)
+    {
+        field = value
+    }
+    var age : Int = _age
+    get() = field
+    set(value)
+    {
+        field = value
+    }
+
+}
+
+fun main()
+{
+    val user1 = User(1, "Kildong", 30)
+    user1.age = 35
+    println("user1.age = ${user1.age}")
+}
