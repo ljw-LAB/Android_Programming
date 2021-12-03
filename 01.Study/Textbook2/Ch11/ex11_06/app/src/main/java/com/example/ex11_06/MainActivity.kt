e1da904f6462af69337934cb30e175592973fd65
ljw-LAB <su5099@naver.com>
2021-12-03 12:58:47 +0900

    Textbook2 16th commit  - Kotlin Programming  Project Commit(11.06 ~ 11.07)

--- 01.Study/Textbook2/Ch11/ex11_06/app/src/main/java/com/example/ex11_06/MainActivity.kt
+++ 01.Study/Textbook2/Ch11/ex11_06/app/src/main/java/com/example/ex11_06/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.ex11_06
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import com.example.ex11_06.databinding.ActiveDrawerBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var toggle: ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.active_drawer)
+
+        val binding = ActiveDrawerBinding.inflate(layoutInflater)
+        toggle = ActionBarDrawerToggle(this, binding.drawer, R.string.app_name, R.string.appbar_scrolling_view_behavior)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toggle.syncState()
+        setContentView(binding.root)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item))
+        {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
