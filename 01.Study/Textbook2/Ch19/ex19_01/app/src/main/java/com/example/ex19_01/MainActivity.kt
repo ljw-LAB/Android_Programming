67dbed1d1f84f89c836c7f010e0af99dbfdd22ff
LEE-NOTEBOOK\이재웅 <su5099@naver.com>
2021-12-28 22:44:45 +0900

    Textbook2 24th commit  - Kotlin Programming  Project Commit(19.01 ~ 19.02)

---
+++ 01.Study/Textbook2/Ch19/ex19_01/app/src/main/java/com/example/ex19_01/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.ex19_01
+
+//import androidx.appcompat.app.AppCompatActivity
+//import android.os.Bundle
+import com.example.ex19_01.databinding.ActivityMainBinding
+//import com.google.android.gms.maps.GoogleMap
+//import com.google.android.gms.maps.OnMapReadyCallback
+//import com.google.android.gms.maps.SupportMapFragment
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.tasks.OnSuccessListener
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    lateinit var binding : ActivityMainBinding
+    var googleMap: GoogleMap? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //setContentView(R.layout.activity_main)
+        (supportFragmentManager.findFragmentById(R.id.mapView) as SupportMapFragment?)!!.getMapAsync(this)
+
+    }
+
+    override fun onMapReady(p0: GoogleMap?) {
+        //TODO("Not yet implemented")
+        googleMap = p0
+    }
+}
