b3a5ff487036ffc3e0aae7ceef7d9571d06c24f1
LEE-NOTEBOOK\이재웅 <su5099@naver.com>
2022-01-02 22:14:39 +0900

    Textbook2 26h commit  - Kotlin Programming  Project Commit(21.01 ~ 21.05)

---
+++ 01.Study/Textbook2/Ch20/ex20_1/app/src/main/java/com/example/ex20_1/AddActivity.kt
@@ -0,0 +1,114 @@
+package com.example.ex20_1
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.ex20_1.databinding.ActivityAddBinding
+import com.example.ex20_1.util.dateToString
+import com.google.firebase.storage.StorageReference
+import java.io.File
+import java.util.*
+
+class AddActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityAddBinding
+    lateinit var filePath: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityAddBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode===10 && resultCode=== RESULT_OK){
+            Glide
+                .with(getApplicationContext())
+                .load(data?.data)
+                .apply(RequestOptions().override(250, 200))
+                .centerCrop()
+                .into(binding.addImageView)
+
+
+            val cursor = contentResolver.query(data?.data as Uri,
+                arrayOf<String>(MediaStore.Images.Media.DATA), null, null, null);
+            cursor?.moveToFirst().let {
+                filePath=cursor?.getString(0) as String
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_add, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId === R.id.menu_add_gallery){
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.setDataAndType(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                "image/*"
+            )
+            startActivityForResult(intent, 10)
+        }else if(item.itemId === R.id.menu_add_save){
+            if(binding.addImageView.drawable !== null && binding.addEditView.text.isNotEmpty()){
+                //store 에 먼저 데이터를 저장후 document id 값으로 업로드 파일 이름 지정
+                saveStore()
+            }else {
+                Toast.makeText(this, "데이터가 모두 입력되지 않았습니다.", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+        return super.onOptionsItemSelected(item)
+    }
+    //....................
+    private fun saveStore(){
+        //add............................
+        val data = mapOf(
+            "email" to MyApplication.email,
+            "content" to binding.addEditView.text.toString(),
+            "date" to dateToString(Date())
+        )
+        MyApplication.db.collection("news")
+            .add(data)
+            .addOnSuccessListener {
+                //store 에 데이터 저장후 document id 값으로 storage 에 이미지 업로드
+                uploadImage(it.id)
+            }
+            .addOnFailureListener {
+                Log.w("kkang", "data save error", it)
+            }
+
+    }
+    private fun uploadImage(docId: String){
+        //add............................
+        val storage = MyApplication.storage
+        //storage 를 참조하는 StorageReference 를 만든다.
+        val  storageRef: StorageReference = storage.reference
+        //실제 업로드하는 파일을 참조하는 StorageReference 를 만든다.
+        val imgRef: StorageReference = storageRef.child("images/${docId}.jpg")
+        //파일 업로드
+        var file = Uri.fromFile(File(filePath))
+        imgRef.putFile(file)
+            .addOnFailureListener {
+                Log.d("kkang"," failure............."+it)
+            }.addOnSuccessListener {
+                Toast.makeText(this, "데이터가 저장되었습니다.",Toast.LENGTH_SHORT).show()
+                finish()
+            }
+
+    }
+}
--- 01.Study/Textbook2/ch21_firebase/src/main/java/com/example/ch20_firebase/MyApplication.kt
+++ 01.Study/Textbook2/ch21_firebase/src/main/java/com/example/ch20_firebase/MyApplication.kt
@@ -1,10 +1,11 @@
-package com.example.ex20_1
+package com.example.ch20_firebase
 
 import androidx.multidex.MultiDexApplication
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.ktx.storage
 
 class MyApplication: MultiDexApplication() {
@@ -12,9 +13,6 @@ class MyApplication: MultiDexApplication() {
     companion object {
         lateinit var auth: FirebaseAuth
         var email: String? = null
-
-        lateinit var  db : FirebaseFirestore
-        lateinit var storage : FirebaseFirestore
 
         fun checkAuth(): Boolean{
             val currentUser = auth.currentUser
@@ -29,13 +27,18 @@ class MyApplication: MultiDexApplication() {
                 false
             }
         }
+
+        lateinit var db: FirebaseFirestore
+        lateinit var storage: FirebaseStorage
+
     }
 
     override fun onCreate() {
         super.onCreate()
         auth =  Firebase.auth
 
-        db = FirebaseFirestore.getInstance()
-        storage = Firebase.storage
+        db=FirebaseFirestore.getInstance()
+        storage=Firebase.storage
+
     }
 }
