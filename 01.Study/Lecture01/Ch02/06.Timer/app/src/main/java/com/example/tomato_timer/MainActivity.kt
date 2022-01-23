d3938080592b61d8c7fe6a6ff72a0319f9452ab7
LEE-NOTEBOOK\이재웅 <su5099@naver.com>
2022-01-23 23:24:17 +0900

    Lecture01 12th commit  - Timer project Commit   1) Project Commit

---
+++ 01.Study/Lecture01/Ch02/06.Timer/app/src/main/java/com/example/tomato_timer/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.tomato_timer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private val remainMinutesTextView: TextView by lazy {
+        findViewById(R.id.remainMinutesTextView)
+    }
+    private val remainSecondsTextView: TextView by lazy {
+        findViewById(R.id.remainSecondsTextView)
+    }
+    private val seekBar: SeekBar by lazy {
+        findViewById(R.id.seekBar)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bindViews()
+    }
+
+    private fun bindViews() {
+        seekBar.setOnSeekBarChangeListener(
+                object : SeekBar.OnSeekBarChangeListener {
+                    override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                        remainMinutesTextView.text = "%02d'".format(progress)
+                    }
+
+                    override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                    }
+
+                    override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+                    }
+                }
+        )
+    }
+}
